3cf519bf8bf7529e10294e5e8e21454454376480
shouta0715 <Myheroes0715@gmail.com>
2024-12-19 23:26:08 +0900

    chore

--- src/main.ts
+++ src/main.ts
@@ -1,108 +1,73 @@
-import './style.css';
 import p5 from 'p5';
 import {
-  ResultsListener,
-  SelfieSegmentation,
-} from '@mediapipe/selfie_segmentation';
-import { Camera } from '@mediapipe/camera_utils';
+  FilesetResolver,
+  ImageSegmenter,
+  ImageSegmenterResult,
+  ImageSegmenterCallback,
+} from '@mediapipe/tasks-vision';
 
+let lastWebcamTime = -1;
 let segmentMask: p5.Image | null = null;
-let segmentImage: p5.Image | null = null;
-let selfieImage: p5.Graphics;
-let p: p5;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-// Webカメラ用video要素生成
-const videoElement = document.createElement('video');
-videoElement.className = 'input_video';
-videoElement.style.display = 'none';
-document.body.appendChild(videoElement);
-
-// セルフィーセグメンテーションの結果ハンドラー
-function onSelfieSegmentationResults(results: {
-  segmentationMask: ImageBitmap;
-  image: ImageBitmap;
-}): ReturnType<ResultsListener> {
-  const _segmentMaskBitMap = results.segmentationMask;
-  const _segmentImageBitMap = results.image;
-
-  if (_segmentImageBitMap && _segmentMaskBitMap && selfieImage) {
-    const ctx = selfieImage.drawingContext as CanvasRenderingContext2D;
-    ctx.save();
-    ctx.clearRect(0, 0, width, height);
-    ctx.drawImage(_segmentMaskBitMap, 0, 0, width, height);
-    ctx.globalCompositeOperation = 'source-in';
-    ctx.drawImage(_segmentImageBitMap, 0, 0, width, height);
-    ctx.restore();
-
-    segmentMask = createImageFromBitmap(_segmentMaskBitMap);
-    segmentImage = createImageFromBitmap(_segmentImageBitMap);
-  }
-}
-
-// SelfieSegmentationインスタンス作成
-const selfieSegmentation = new SelfieSegmentation({
-  locateFile: (file: string) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1/${file}`;
-  },
-});
-
-selfieSegmentation.setOptions({
-  modelSelection: 1,
-});
-selfieSegmentation.onResults(onSelfieSegmentationResults as ResultsListener);
-
-// カメラ設定
-const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await selfieSegmentation.send({ image: videoElement });
-  },
-  width,
-  height,
-});
-camera.start();
-
-// ImageBitmapからp5.Imageを生成するヘルパー関数
-function createImageFromBitmap(bitmap: ImageBitmap): p5.Image {
-  const imgG = p.createGraphics(bitmap.width, bitmap.height);
-  (imgG.drawingContext as CanvasRenderingContext2D).drawImage(bitmap, 0, 0);
-  const pImg = p.createImage(bitmap.width, bitmap.height);
-  pImg.copy(
-    imgG,
-    0,
-    0,
-    bitmap.width,
-    bitmap.height,
-    0,
-    0,
-    bitmap.width,
-    bitmap.height
+const createImageSegmenter = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm'
   );
-  return pImg;
-}
 
+  return await ImageSegmenter.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath:
+        'https://storage.googleapis.com/mediapipe-models/image_segmenter/selfie_segmenter/float16/1/selfie_segmenter.tflite',
+      delegate: 'GPU',
+    },
+    outputCategoryMask: true,
+    runningMode: 'VIDEO',
+  });
+};
+
+let p: p5;
+
+const callbackForVideo: ImageSegmenterCallback = (
+  result: ImageSegmenterResult
+) => {
+  // TODO: 画像に変換する。
+};
+
+// p5.jsスケッチ
 const sketch = (_p: p5) => {
   p = _p;
-  p.setup = () => {
+  let imageSegmenter: ImageSegmenter | null = null;
+
+  let video: HTMLVideoElement | null = null;
+
+  let videoTime = -1;
+  p.setup = async () => {
     p.createCanvas(width, height);
-    selfieImage = p.createGraphics(width, height);
+
+    imageSegmenter = await createImageSegmenter();
+    const cap = p.createCapture('video');
+    cap.hide();
+    video = cap.elt;
   };
 
-  p.draw = () => {
+  p.draw = async () => {
     p.background(255);
+    if (!video) return;
+    if (!imageSegmenter) return;
+    if (!video.videoWidth || !video.videoWidth) return;
+    if (video.currentTime === videoTime) return;
 
-    p.push();
+    lastWebcamTime = video.currentTime;
 
-    if (segmentMask && segmentImage) {
-      p.push();
-      p.scale(-1, 1);
-      p.image(segmentMask, -width, 0, width, height);
-      p.pop();
+    imageSegmenter.segmentForVideo(video, video.currentTime, callbackForVideo);
+    videoTime = video.currentTime;
+
+    if (segmentMask) {
+      p.image(segmentMask, 0, 0, width, height);
     }
-
-    p.pop();
   };
 };
 
